de014a1452b517486d3d71dc4e20427d45ae4050
s.alex <s.alex.developer@gmail.com>
2023-09-22 15:50:04 +0300

    Домашнее задание 1."Создание проекта"

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,15 +1,20 @@
+import os
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
 
 
 def home_view(request):
+
     template_name = 'app/home.html'
+
     # впишите правильные адреса страниц, используя
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
     # context и параметры render менять не нужно
@@ -17,19 +22,36 @@ def home_view(request):
     context = {
         'pages': pages
     }
+
     return render(request, template_name, context)
 
 
 def time_view(request):
+
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+
+    current_time = datetime.now().time()
+
     msg = f'Текущее время: {current_time}'
+
     return HttpResponse(msg)
 
 
 def workdir_view(request):
+
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+
+    dir_name = 'app'
+
+    template_name = 'app/workdir.html'
+
+    list_dir = os.listdir(path=dir_name)
+
+    context = {
+        'dir_name': dir_name,
+        'list_dir':  list_dir
+    }
+    return render(request, template_name, context)
