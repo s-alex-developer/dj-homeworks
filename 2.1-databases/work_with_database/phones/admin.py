0f54bcd60486dfdc488153b2ea155636829afdd9
s.alex <s.alex.developer@gmail.com>
2023-10-05 17:05:57 +0300

    Внес небольшие доработки.

--- 2.1-databases/work_with_database/phones/admin.py
+++ 2.1-databases/work_with_database/phones/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from phones.models import Phone
+
+
 # Register your models here.
+
+"""
+    http://127.0.0.1:8000/admin/  
+    user: admin  
+    password: admin
+"""
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'image', 'release_date', 'lte_exists', 'slug']
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Phone(models.Model):
 
     # TODO: Добавьте требуемые поля
-    name = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=7, decimal_places=1)
-    image = models.CharField(max_length=300)
-    release_date = models.DateField()
-    lte_exists = models.BooleanField()
-    slug = models.SlugField()
+    name = models.CharField('Модель', max_length=50)
+    price = models.DecimalField('Стоимость', max_digits=7, decimal_places=1)
+    image = models.CharField('Изображение', max_length=255)
+    release_date = models.DateField('Дата релиза', null=True)
+    lte_exists = models.BooleanField('Поддержка LTE', null=True)
+    slug = models.SlugField('Слизняк :)', unique=True, db_index=True)
